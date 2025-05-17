51f46eb9f3d0ce3341db26559ac494c68b45faa8
elicett <eliezerlicett@gmail.com>
2025-05-17 18:56:04 -0400

    Part1, part.b (Javascript) - Step 5 completed

--- part1/part.b/src/App.jsx
+++ part1/part.b/src/App.jsx
@@ -1,4 +1,4 @@
-// Part b: Javascript, Step 1.4
+// Part b: Javascript, Step 1.5
 const Header = (props) => {
   return (
     <div>
@@ -34,27 +34,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={parts} />
-      <Total parts={parts}/>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
