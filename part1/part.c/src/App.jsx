7845cfb337bd38086292ebde8fe119aff54f2b28
elicett <eliezerlicett@gmail.com>
2025-05-18 05:27:47 -0400

    Added part c: Component State, events controllers

---
+++ part1/part.c/src/App.jsx
@@ -0,0 +1,44 @@
+// Part 1.c: Component state, events controllers
+import { useState } from 'react'
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => { 
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
+    </div>
+  )
+}
+
+export default App
