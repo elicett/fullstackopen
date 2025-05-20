cded225caab16cc49159fcc412c61ed238aaa5c5
elicett <eliezerlicett@gmail.com>
2025-05-20 08:52:02 -0400

    Added part1, subpart d: Exercise 1.8: Unicafe, step3

--- part1/part.d/src/App.jsx
+++ part1/part.d/src/App.jsx
@@ -1,53 +1,5 @@
-// Part 1, subpart d. Exercise 1.7: Unicafe, step 2
-import { useState } from 'react'
-
-
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  return (  
-    <div>
-      <h1>give feedback</h1>
-      <button onClick={() => setGood(good+1)}> good </button>
-      <button onClick={() => setNeutral(neutral+1)}> neutral </button>
-      <button onClick={() => setBad(bad+1)}> bad </button>
-      <h1>statistics</h1>
-      <p>good={good}</p>
-      <p>neutral={neutral}</p>
-      <p>bad={bad}</p>
-      <p>total={good+neutral+bad}</p>
-      <p>average={(good*1+neutral*0+bad*-1)/(good+neutral+bad)}</p>
-      <p>positive={((good/(good+neutral+bad))*100)}%</p>
-    </div>
-  )
-}
-
-export default App
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 // Part 1, subpart d. Exercise 1.8: Unicafe, step 3
 import { useState } from 'react'
-
 
 const Statistics = ({ good, neutral, bad }) => {
 
@@ -86,4 +38,3 @@ const App = () => {
 }
 
 export default App
-*/
