3fc342096b0d6fbb28b23321526e6f9ec81654a0
elicett <eliezerlicett@gmail.com>
2025-05-20 08:50:54 -0400

    Added part1, subpart d: Exercise 1.7: Unicafe, step2

--- part1/part.d/src/App.jsx
+++ part1/part.d/src/App.jsx
@@ -1,49 +1,89 @@
-// Part 1, subpart d. Exercise 1.6: Unicafe
+// Part 1, subpart d. Exercise 1.7: Unicafe, step 2
 import { useState } from 'react'
+
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-
-
-
-  const handleClick = () => {
-    console.log('clicked good')
-  }
-
-  return (
+  return (  
     <div>
-      <div>
-        <h1>give feedback</h1>
-        <button onClick={() => setGood(good+1)}> good </button>
-        <button onClick={() => setNeutral(neutral+1)}> neutral </button>
-        <button onClick={() => setBad(bad+1)}> bad </button>
-      </div>
-      <div>
-        <h1>statistics</h1>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {good+neutral+bad}</p>
-        <>Average ={(good*1+neutral*0+bad*-1)/(good+neutral+bad)}</>
-        <p>positive {((good)/(good+neutral+bad)*100)}%</p>
-      </div>
-
-
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good+1)}> good </button>
+      <button onClick={() => setNeutral(neutral+1)}> neutral </button>
+      <button onClick={() => setBad(bad+1)}> bad </button>
+      <h1>statistics</h1>
+      <p>good={good}</p>
+      <p>neutral={neutral}</p>
+      <p>bad={bad}</p>
+      <p>total={good+neutral+bad}</p>
+      <p>average={(good*1+neutral*0+bad*-1)/(good+neutral+bad)}</p>
+      <p>positive={((good/(good+neutral+bad))*100)}%</p>
     </div>
   )
 }
 
 export default App
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
-Como la mayoría de las empresas, Unicafe recopila comentarios de sus clientes. Tu tarea es implementar una aplicación web para
-recopilar comentarios de los clientes. Solo hay tres opciones para los comentarios: good (bueno), neutral y bad(malo).
+// Part 1, subpart d. Exercise 1.8: Unicafe, step 3
+import { useState } from 'react'
 
-La aplicación debe mostrar el número total de comentarios recopilados para cada categoría. Tu aplicación final podría verse así:
 
-Ten en cuenta que tu aplicación debe funcionar solo durante una única sesión del navegador. Una vez que se actualice la página, 
-los comentarios recopilados pueden desaparecer.
+const Statistics = ({ good, neutral, bad }) => {
+
+  const total = good+neutral+bad
+  const average = (good*1+neutral*0+bad*-1)/(good+neutral+bad)
+  const positive = ((good/total)*100)
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good={good}</p>
+      <p>neutral={neutral}</p>
+      <p>bad={bad}</p>
+      <p>total={total}</p>
+      <p>average={average}</p>
+      <p>positive={positive}%</p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (  
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good+1)}> good </button>
+      <button onClick={() => setNeutral(neutral+1)}> neutral </button>
+      <button onClick={() => setBad(bad+1)}> bad </button>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+export default App
 */
