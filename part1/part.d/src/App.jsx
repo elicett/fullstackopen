5c328c5100c3a41054381184ce3c09bfeb5d4ba8
elicett <eliezerlicett@gmail.com>
2025-05-22 10:37:40 -0400

    Part1, sybpart d. Exercise 1.12: Anecdotes, step 1

--- part1/part.d/src/App.jsx
+++ part1/part.d/src/App.jsx
@@ -1,91 +1,37 @@
-// Part 1, subpart d. Exercise 1.11: Unicafe, step 6 (Corrected Buttom Function and commited again)
+// Part 1, subpart d. Exercise 1.12: anecdotes, step 1
 import { useState } from 'react'
 
-
-const StatisticLine = ({ text, value }) =>{
-  return (
-    <div>
-      {text}: {value}
-    </div>
-  )
-
+const generateRandom = () => {
+  const min = 0;
+  const max = 7;
+    return (
+      Math.floor(Math.random() * (max - min + 1) + min)
+    )
 }
 
-const Statistics = (props) => {
-  const good=props.good
-  const neutral=props.neutral
-  const bad=props.bad
-  const all = good+neutral+bad
-  if (all === 0) return (<p>No feedback given</p>)
-  const average = (good*1+neutral*0+bad*-1)/(good+neutral+bad)
-  const positive = ((good/all)*100)  + '%'
-
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th colSpan={2}><h1>statistics</h1></th>
-        </tr>
-      </thead>
-
-      <tbody>
-        <tr>
-          <td>good</td>
-          <td>{good}</td>
-        </tr>
-        <tr>
-          <td>neutral</td>
-          <td>{neutral}</td>
-        </tr>
-        <tr>
-          <td>bad</td>
-          <td>{bad}</td>
-        </tr>
-        <tr>
-          <td>all</td>
-          <td>{all}</td>
-        </tr>
-        <tr>
-          <td>average</td>
-          <td>{average}</td>
-        </tr>
-        <tr>
-          <td>positive</td>
-          <td>{positive}</td>
-        </tr>
-      </tbody>
-    </table>
-  )
-}
-
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>    
-    {text}  
-  </button>
-)
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-  const handleClicksetGood = () => {
-    setGood(good+1)
-  }
-  const handleClicksetNeutral = () => {
-    setNeutral(neutral+1)
-  }
-  const handleClicksetBad = () => {
-    setBad(bad+1)
+  const [selected, setSelected] = useState(0)
+
+  const handleSelected = () => {
+    setSelected(generateRandom())
   }
 
-  return (  
+  return (
     <div>
-      <h1>give feedback</h1>
-      <Button handleClick={handleClicksetGood} text='good'/>
-      <Button handleClick={handleClicksetNeutral} text='neutral'/>
-      <Button handleClick={handleClicksetBad} text='bad' />
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={handleSelected}>next anecdote</button>
     </div>
   )
 }
