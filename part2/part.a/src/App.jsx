b17b33ebaf85011e11a7e153901551bc4f22f66c
elicett <eliezerlicett@gmail.com>
2025-05-25 03:17:15 -0400

    Part2, subpart a. Rendering one colection, modules. Exercise 2.4, (step 9)

--- part2/part.a/src/App.jsx
+++ part2/part.a/src/App.jsx
@@ -1,83 +1,85 @@
-//Part 2, subpart a. Rendering one colection, modules. Exercise 2.3, (step 8):
+//Part 2, subpart a. Rendering one colection, modules. Exercise 2.4, (step 9):
 import Note from './components/Note'
 
 
-const Course = (props) => {
-  return(
-    <div>
-        <Header course={props.course.name}></Header>
-        <Content course={props.course}></Content>
-    </div>
-  )
-}
-
-const Header = (props) => {
-    return (
-      <div>
-        <h1>{props.course}</h1>
-      </div>
-    )
-  }
-
-const Content = (props) => {
-  return(
-    <div>
-      <Part course={props.course.parts}></Part>
-    </div>
-  )
-}
-
 const Part = (props) => {
+  //const { name, parts } = course;
+  const name = props.course.name
+  const parts = props.course.parts
+  const totalExercisesPerPart = parts.reduce((sum, part) => sum + part.exercises, 0);
 
-  const identifierArray = props.course
-  //identifierArray.forEach((parts) => console.log(parts.name,":", parts.exercises))
-  const miLista = identifierArray.map(parts => (
-    <p key={parts.id}>{parts.name}: {parts.exercises}</p>
-  ))
-  
-  const exercisesArray = props.course
-  console.log("exercisesArray:", exercisesArray)
-  const acumulador=0
-  const totalExercises = exercisesArray.reduce((acumulador, valorActual) =>{
-    return acumulador = acumulador + valorActual.exercises
-  }, 0)
-
-  return(
+  return (
     <div>
-      <li>{miLista}</li>
-      <p><strong>Total of {totalExercises} exercises</strong></p>
+      <h2>{name}</h2>
+      {parts.map(part => (
+        <p key={part.id}>  {part.name}: {part.exercises} </p>
+      ))}
+      <p>Total of {totalExercisesPerPart} exercises</p>
     </div>
-  )
-}
+  );
+};
+
+
+const Course = (props) => {
+  const courses = props.courses
+  return (
+    <>
+      <h1>Web development curriculum</h1>
+      {courses.map(course => (
+        <Part key={course.id} course={course} />
+      ))}
+    </>
+  );
+};
+
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }  
-    ]
-  }
-  return <Course course={course} />
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return <Course courses={courses} />
 }
 
 export default App
