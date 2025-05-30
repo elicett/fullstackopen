854576b5a641dd70b7fa69ee1ef164f5fc44f35c
elicett <eliezerlicett@gmail.com>
2025-05-30 10:01:35 -0400

    Part 2, subpart b. Fornm Exercise 2.9, (step 4): Search bar

--- part2/part.b/src/App.jsx
+++ part2/part.b/src/App.jsx
@@ -1,25 +1,33 @@
-//Part 2, subpart b. Forms Exercise 2.7, (step 2):
+//Part 2, subpart b. Forms Exercise 2.9, (step 4):
 import { useState } from 'react'
 
 const App = () => {
   const [contacts, setContacts] = useState([
-    { name: 'Arto Hellas' , number: '39-44-5323523'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [finder, setFinder] = useState('')
+
 
   //Funciones
+
+  //Funcion para añadir registro
   const addValues = (event) => {
     event.preventDefault()
-    const contactObject = {
-      name: newName,
-      number: newNumber
-    }
-    
+
     if (contacts.map(valores => valores.name).includes(newName)===true){
       alert('The contact "'+newName+'" already exists')
       event.preventDefault()
       return
+    }
+
+    const contactObject = {
+      name: newName,
+      number: newNumber
     }
 
     setContacts(contacts.concat(contactObject))
@@ -27,31 +35,47 @@ const App = () => {
     setNewNumber('')
   }
 
+  //Funcion para mostrar registro buscado
+  const contactsToShow = contacts.filter(
+    cFiltered => cFiltered.name.toLowerCase().includes(finder.toLowerCase())
+  )
+
+
   //Handlers
   const handleAddName = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
 
-    const handleAddNumber = (event) => {
+  const handleAddNumber = (event) => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
   }
 
+  const handleFinder = (event) => {
+    console.log(event.target.value)
+    //toLowerCase()
+    setFinder(event.target.value)
+  }
+
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div> filter show with <input value={finder} onChange={handleFinder}/></div>
+
+      <h2>add a new</h2>
       <form onSubmit={addValues}>
         <div>name: <input value={newName} onChange={handleAddName}/></div>
         <div>number: <input value={newNumber} onChange={handleAddNumber}/></div>
         <div><button type="submit">add</button></div>
-        
       </form>
+
       <h2>Numbers</h2>
         <div>
-          {contacts.map(valores =>
-            <p key={valores.name}>
-              {valores.name}: {valores.number}
+          {contactsToShow.map(contact =>
+            <p key={contact.id}> 
+              {contact.name} {contact.number}
             </p>
           )}
         </div>
@@ -60,3 +84,4 @@ const App = () => {
 }
 
 export default App
+
