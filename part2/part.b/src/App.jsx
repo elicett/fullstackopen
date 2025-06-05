ae620addfe5b56f36ec66e3b4dde7b3d8a924808
elicett <eliezerlicett@gmail.com>
2025-06-05 01:39:03 -0400

    Part 2, subpart b. Obtaining data from the server, Exercise 2.11 (step 6)

--- part2/part.b/src/App.jsx
+++ part2/part.b/src/App.jsx
@@ -1,120 +1,36 @@
-//Part 2, subpart b. Forms Exercise 2.10, (step 5):
-import { useState } from 'react'
-
-
-
-const Filter = ( {handleFinder, finder} ) => {
-  return (
-    <div>
-      <h1>
-      Phonebook
-      </h1>
-      <form>
-        <div>filter show with:<input value={finder} onChange={handleFinder}></input></div>
-      </form>
-    </div>
-  )
-}
-
-
-const PersonForm = ( {handleAddName, handleAddNumber, newName, handleSetContacts, newNumber} ) => {
-
-  return (
-    <div>
-      <h1>
-        add a new
-      </h1>
-      <form  onSubmit={handleSetContacts}>
-        <div>name:<input value={newName} onChange={handleAddName}></input></div>
-        <div>number:<input value={newNumber} onChange={handleAddNumber}></input></div>
-        <div><button type="submit" >add</button></div>
-      </form>
-    </div>  
-  )
-}
-
-
-const Persons = ({ contacts, finder }) => {
-  const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(finder.toLowerCase())
-  );
-  const contactItems = filteredContacts.map(contact => (
-    <p key={contact.id}>
-      {contact.name} {contact.number}
-    </p>
-  ));
-
-  return (
-    <div>
-      <h2>Numbers</h2>
-      {contactItems.length > 0 ? contactItems : <p>No contacts found.</p>}
-    </div>
-  );
-};
-
+//Part 2, subpart b. Obtaining data from the server, Exercise 2.11, (step 6):
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 
 const App = () => {
-  const [contacts, setContacts] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [finder, setFinder] = useState('')
+  const [persons, setPersons] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState('true')
 
-
-  //Funciones
-
-  //Funcion para añadir registro
-
-  //Handlers
-
-  const handleSetContacts = (event) => {
-    event.preventDefault()
-    console.log("Se ejecutó handleSetContacts")
-    if (contacts.map(valores => valores.name).includes(newName)===true){
-      alert('The contact "'+newName+'" already exists')
-      event.preventDefault()
-      return
-    }
-    const newObject = {
-      name: newName,
-      number: newNumber,
-      id: contacts.length + 1,
-    }
-    setContacts(contacts.concat(newObject))
-    setNewName('')
-    setNewNumber('')
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
   }
 
-  const handleAddName = (event) => {
-    event.preventDefault()
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
+  useEffect(hook, [])
 
-  const handleAddNumber = (event) => {
-    console.log(event.target.value)
-    setNewNumber(event.target.value)
-  }
-
-  const handleFinder = (event) => {
-    //toLowerCase()
-    setFinder(event.target.value)
-    console.log(event.target.value)
-  }
+  console.log('render', persons.length, 'contacts')
 
   return (
     <div>
-      <Filter contacts={contacts} handleAddName={handleAddName} handleFinder={handleFinder} handleAddNumber={handleAddNumber} handleSetContacts={handleSetContacts} newName={newName} newNumber={newNumber} finder={finder}/>
-      <PersonForm contacts={contacts} handleAddName={handleAddName} handleAddNumber={handleAddNumber} handleSetContacts={handleSetContacts} newName={newName} newNumber={newNumber}/>
-      <Persons contacts={contacts} finder={finder}/>
+      {persons.map(cPersons => (
+        <p key={cPersons.id}>{cPersons.name} {cPersons.number}</p>
+      ))}
     </div>
-  )
+    )
 }
+
 
 export default App
--- part2/part.b/src/main.jsx
+++ part2/part.b/src/main.jsx
@@ -1,11 +1,6 @@
-//Part 2, subpart b. Forms Exercise 2.10, (step 5):
+//Part 2, subpart b. Forms Exercise 2.11, (step 6):
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import App from './App'
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
